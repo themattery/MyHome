f2c68fbbc1a1b84fbdc7c6077fde01beb89f672a
Matheus Barbosa <115056575+themattery@users.noreply.github.com>
2026-01-07 20:17:38 -0300

    Projeto inicial MyHome -> estrutura base

---
+++ src/br/edu/ifpb/myhome/anuncio/Anuncio.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.myhome.anuncio;
+
+public class Anuncio {
+
+    // private EstadoAnuncio estado;
+    public String titulo;
+    public String descricao;
+
+
+    public void submeter() {
+
+    }
+
+    public void mudarEstado(/* EstadoAnuncio novoEstado */) {
+
+    }
+
+}
---
+++ src/br/edu/ifpb/myhome/app/Main.java
@@ -0,0 +1,6 @@
+package br.edu.ifpb.myhome.app;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
